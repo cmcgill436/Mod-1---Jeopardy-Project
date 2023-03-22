9447abee345b1b599ad222158144c36ef87627c6
cmcgill436 <candra.mcgill@gmail.com>
2023-03-21 19:41:05 -0500

    update

--- project-jeopardy/js/script.js
+++ project-jeopardy/js/script.js
@@ -1,6 +1,13 @@
 /*
 -- increment players when button is clicked
--- start when play button is clicked
-  - transition to another screen after settings are created (gameboard)
-
+-- when on question screen, 30 sec timer needs to be set and Think! song can be played 
+  -- decide whether answer can be typed in or make questions multiple choice
+--gameboard
+  -- specific question box disappears after an attempt has been made
+  figure out how to fill in the gameboard with transparent boxs that can be selected 
+--questions
+  -determine if questions will be assigned randomly or if they will get harder as the value increases
+-- reset/play again/quit
+  --'X' button that quits/let the player restart/get to the home screen to play the game
+    -- ensure the game resets when it is exited out of.
 */
